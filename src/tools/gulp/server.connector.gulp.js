1c8ed1ef92ddab8b746275045e7e1e5ff6bf7522
Jan Age Lavik <jan.lavik@knowit.no>
2017-09-21 16:34:02 +0200

    Add server middleware to test OIDC callback

--- src/tools/gulp/server.connector.gulp.js
+++ src/tools/gulp/server.connector.gulp.js
@@ -7,12 +7,50 @@ const PUBLIC_FOLDER = path.resolve( __dirname, ROOT, 'src/main/public' );
 
 const gulp = require( 'gulp' );
 const connect = require( 'gulp-connect' );
+const bodyParser = require( 'body-parser' );
+const url = require( 'url' );
 
 const packageJson = require( path.resolve( __dirname, ROOT, 'package.json' ) );
 
-gulp.task( 'connector:server', ['connector'], () => {
+
+/**
+ * @param {RegExp} pathRegExp
+ * @param {ServerRouterMiddlewareCallback} callback
+ * @return {Function}
+ */
+function generateMiddleware( pathRegExp, callback ) {
+    return ( req, res, next ) => {
+        const urlParsed = url.parse( req.url );
+        if ( pathRegExp.test( urlParsed.pathname ) ) {
+            const match = ( urlParsed.pathname.match( pathRegExp ) || [null] ).slice( 1 );
+            callback.apply( null, [req, res, next].concat( match ) );
+        }
+        else {
+            next();
+        }
+    };
+}
+
+gulp.task( 'connector:server', () => {
+    let middlewareList = [];
+
+    // Handle OIDC /callback
+    middlewareList.push( generateMiddleware( /callback/, ( req, res, next ) => {
+        // const queryObject = url.parse( req.url,true ).query;
+
+        res.statusCode = 200;
+        res.end( 'OK' );
+
+        next();
+    } ) );
+
+    middlewareList.push( bodyParser.json() );
+
     return connect.server( {
         port: packageJson.port,
         root: PUBLIC_FOLDER,
+        middleware: () => {
+            return middlewareList;
+        }
     } );
 } );
