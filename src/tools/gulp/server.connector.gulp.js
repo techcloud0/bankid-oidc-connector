68f908cdc3aa3f97b89b756e15bad2a40122b727
Jan Age Lavik <jan.lavik@knowit.no>
2018-01-26 15:23:11 +0100

    Correct server connector filepath to callback example

--- src/tools/gulp/server.connector.gulp.js
+++ src/tools/gulp/server.connector.gulp.js
@@ -14,6 +14,7 @@ const mime = require( 'mime' );
 const responseFormPostMiddleware = require( '../server/response-form-post.server-middleware' );
 const oauthServerMiddleware = require( '../server/oauth.server-middleware' );
 
+const callbackPageFile = path.resolve( PUBLIC_FOLDER, 'example_callback_page.html' );
 const packageJson = require( path.resolve( __dirname, ROOT, 'package.json' ) );
 
 
@@ -42,47 +43,14 @@ gulp.task( 'connector:server', () => {
 
     // Handle OIDC /callback
     middlewareList.push( generateMiddleware( /callback/, ( req, res, next ) => {
-        // const queryObject = url.parse( req.url,true ).query;
-
-        // 1 authorization_flow   response_type=code
-        // https://openid.net/specs/openid-connect-core-1_0.html#CodeFlowAuth
-        //
-        // HTTP/1.1 302 Found
-        // Location: https://client.example.org/cb?
-        //     code=SplxlOBeZQQYbYS6WxSbIA
-        //     &state=af0ifjsldkj
-
-
-        // 2 Implicit flow   response_type=id_token token
-        // https://openid.net/specs/openid-connect-core-1_0.html#ImplicitFlowAuth
-        //
-        // HTTP/1.1 302 Found
-        // Location: https://client.example.org/cb#
-        //     access_token=SlAV32hkKG
-        //     &token_type=bearer
-        //     &id_token=eyJ0 ... NiJ9.eyJ1c ... I6IjIifX0.DeWt4Qu ... ZXso
-        // &expires_in=3600
-        // &state=af0ifjsldkj
-
-        // 3 Hybrid flow  response_type=code id_token
-        // https://openid.net/specs/openid-connect-core-1_0.html#HybridFlowAuth
-        //
-        // HTTP/1.1 302 Found
-        // Location: https://client.example.org/cb#
-        //     code=SplxlOBeZQQYbYS6WxSbIA
-        //     &id_token=eyJ0 ... NiJ9.eyJ1c ... I6IjIifX0.DeWt4Qu ... ZXso
-        // &state=af0ifjsldkj
-
-        // Could actually just return the examples/result.html
-        const filePath = path.resolve( PUBLIC_FOLDER, 'examples/example_callback_page.html' );
-        fs.readFile( filePath, 'UTF-8', ( err, result ) => {
+        fs.readFile( callbackPageFile, 'UTF-8', ( err, result ) => {
             if ( err ) {
-                console.error( 'File \'%s\' does not exist', filePath );
+                console.error( 'File \'%s\' does not exist', callbackPageFile );
                 res.statusCode = 400;
-                res.end( `File '${path.relative( PUBLIC_FOLDER, filePath )}' does not exist` );
+                res.end( `File '${path.relative( PUBLIC_FOLDER, callbackPageFile )}' does not exist` );
             }
             else {
-                const mimeType = mime.getType( filePath );
+                const mimeType = mime.getType( callbackPageFile );
 
                 if ( res.locals && res.locals.form_data ) {
                     result = result.replace( /{\w+}/g, JSON.stringify( res.locals.form_data ) );
@@ -90,7 +58,7 @@ gulp.task( 'connector:server', () => {
 
                 res.writeHead( 200, { 'Content-Type': mimeType } );
                 if ( /^image\//.test( mimeType || '' ) ) {
-                    fs.createReadStream( filePath ).pipe( res );
+                    fs.createReadStream( callbackPageFile ).pipe( res );
                 }
                 else {
                     res.end( result );
