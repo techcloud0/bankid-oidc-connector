dd28dea417f5fd662b5913c26d8cfe2f36e70849
Jan Age Lavik <jan.lavik@knowit.no>
2018-01-29 13:57:22 +0100

    Add maven build task

---
+++ src/tools/gulp/maven.connector.gulp.js
@@ -0,0 +1,26 @@
+const gulp = require( 'gulp' );
+const path = require( 'upath' );
+
+const ROOT = path.resolve( __dirname, '../../../' );
+
+const maven = require( 'maven-deploy' );
+const maven_config = require( path.resolve( ROOT, './maven-config.json' ) );
+
+/**
+ * Deploys a snapshot version of the next release (that is version in package.json + 1) in the
+ * nexus repo defined in maven.config.json.
+ * <br>
+ * Uses this plugin: https://github.com/finn-no/maven-deploy
+ */
+gulp.task( 'connector:dist:deploy', ['connector:dist'], () => {
+    maven.config( maven_config );
+    maven.deploy( 'bankid-public-snapshot', true );
+} );
+
+/**
+ * Deploy a release version of bankid-oidc-connector with the version given in package.json
+ */
+gulp.task( 'connector:dist:release', ['connector:dist'], () => {
+    maven.config( maven_config );
+    maven.deploy( 'bankid-public' );
+} );
